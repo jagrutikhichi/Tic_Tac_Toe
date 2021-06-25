6e577407ec220c54ee2f0ec4115754014639fe58
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-06-25 09:24:46 +0530

    Added: Created Tic Tac Toe Bored and Modified

--- src/com/bridgelab/tictactoe/TicTacToe.java
+++ src/com/bridgelab/tictactoe/TicTacToe.java
@@ -10,11 +10,7 @@ public class TicTacToe {
 		char bored[] = new char [N];
 		for (int i= 0;i < 9;i++) {
 			bored[i] = '-';
-		}
-		System.out.println(bored[0]+"|"+bored[1]+"|"+bored[2]);
-		System.out.println(bored[3]+"|"+bored[4]+"|"+bored[5]);
-		System.out.println(bored[6]+"|"+bored[7]+"|"+bored[8]);
-		
+		}		
 	}
 
 }
