bef4718d472642417526791ef47cf401a7768ddb
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-06-25 10:32:49 +0530

    Added: Asked User to Choose Letter

--- src/com/bridgelab/tictactoe/TicTacToe.java
+++ src/com/bridgelab/tictactoe/TicTacToe.java
@@ -1,16 +1,35 @@
 package com.bridgelab.tictactoe;
 
+import java.util.*;
 public class TicTacToe {
 	public static void main(String[] args) {
-		CreateBored();
-		
+		createBored();
+		userChoice();
 	}
-	public static void CreateBored() {
-		int N = 9;
-		char bored[] = new char [N];
+	public static void createBored() {
+		char [] bored = new char [9];
 		for (int i= 0;i < 9;i++) {
 			bored[i] = '-';
-		}		
+		}
+	}
+	public static void userChoice() {
+		char player = ' ';
+		char computer = ' ';
+		Scanner s = new Scanner(System.in);
+		System.out.println("Choose a Letter 1.X or 2.O :");
+		int userInput = s.nextInt();
+		switch (userInput) {
+		case 1 :
+			player = 'X';
+			computer = 'O';
+			System.out.println("You have Taken Letter : "+player);
+			break;
+		case 2 :
+			player = 'O';
+			computer = 'X';
+			System.out.println("You have Taken Letter : "+player);
+			break;
+		}
 	}
 
 }
