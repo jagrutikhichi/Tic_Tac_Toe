410de2402cd98ec955f312cd93cb81ad92034df8
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-06-26 23:18:11 +0530

    Added: Draw bored Method

--- src/com/bridgelab/tictactoe/TicTacToe.java
+++ src/com/bridgelab/tictactoe/TicTacToe.java
@@ -3,33 +3,39 @@ package com.bridgelab.tictactoe;
 import java.util.*;
 public class TicTacToe {
 	public static void main(String[] args) {
+		
 		createBored();
-		userChoice();
-	}
-	public static void createBored() {
-		char [] bored = new char [9];
-		for (int i= 0;i < 9;i++) {
-			bored[i] = '-';
-		}
-	}
-	public static void userChoice() {
+		Scanner userInput = new Scanner(System.in);
+		char userLetter = userChoice(userInput);
 		char player = ' ';
 		char computer = ' ';
-		Scanner s = new Scanner(System.in);
-		System.out.println("Choose a Letter 1.X or 2.O :");
-		int userInput = s.nextInt();
-		switch (userInput) {
-		case 1 :
+		
+		if (userLetter == 'X') {
 			player = 'X';
 			computer = 'O';
 			System.out.println("You have Taken Letter : "+player);
-			break;
-		case 2 :
+		}
+		else if (userLetter == 'Y') {
 			player = 'O';
 			computer = 'X';
 			System.out.println("You have Taken Letter : "+player);
-			break;
 		}
+		else {
+			System.out.println("Invalid Input");
+		}
+	}
+	public static void createBored() {
+		int [] bored = new int [9];
+		for (int i= 0;i < 9;i++) {
+			bored[i] = i+1 ;
+		}
+		System.out.println(bored[0]+"|"+bored[1]+"|"+bored[2]);
+		System.out.println(bored[3]+"|"+bored[4]+"|"+bored[5]);
+		System.out.println(bored[6]+"|"+bored[7]+"|"+bored[8]);
+	}
+	private static char userChoice(Scanner userInput) {
+		System.out.println("Choose a Letter X or O :");
+		return userInput.next().toUpperCase().charAt(0);
 	}
 
 }
